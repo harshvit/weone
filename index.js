63a0534b6edea9e95774ba1c3c6fd6c221ba7d49
hmharshith <hmharshith@gmail.com>
2017-11-07 14:17:04 +0530

    first ammu appu commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var exp = require('express');
+var app = exp();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(exp.static('client/'));
+
+app.get('/' , function(req , res){
+	res.sendFile('index.html' , {root: "client/"});
+});
+
+
+http.listen(3000 , function(){
+	console.log("Started WeOne server at port 3000");
+});
+
+io.on('connection' , function(socket){
+	console.log("A User is connected !");
+	
+	socket.on('appu messaged' , function(msg){
+		console.log("Message : "+msg);
+		io.emit('ammu received' , msg);
+	});
+	
+	socket.on('disconnect' , function(){
+		console.log('User disconnected !');
+	});
+});
